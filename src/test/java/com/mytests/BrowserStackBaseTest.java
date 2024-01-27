328a739ef6d08b840200ce84208ce38f094d5b52
SumanthPaiAutomation <sumanthpai777@gmail.com>
2024-01-27 11:17:48 +0530

    some driver initialization correction

--- src/test/java/com/mytests/BrowserStackBaseTest.java
+++ src/test/java/com/mytests/BrowserStackBaseTest.java
@@ -15,6 +15,9 @@ import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.ITestResult;
@@ -69,12 +72,15 @@ public class BrowserStackBaseTest {
             switch (browserName) {
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
+                    driver= new ChromeDriver();
                     break;
                 case "firefox":
                     WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
                     break;
                 case "edge":
                     WebDriverManager.edgedriver().setup();
+                    driver= new EdgeDriver();
                     break;
 
                 default:
